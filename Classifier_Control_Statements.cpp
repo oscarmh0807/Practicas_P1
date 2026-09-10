d8bd50188489367c6b2d9e1a6e55e8232dc790fb
Oscar Huitrón <oscarmh0807@gmail.com>
2026-09-10 02:25:16 -0600

    Fixed Control statements for grades

--- Classifier_Control_Statements.cpp
+++ Classifier_Control_Statements.cpp
@@ -6,38 +6,38 @@ int main()
 {
     double calificacion;
 
-    cout << "Ingrese una calificación de 0 a 100: ";
+    cout << "Ingrese una calificacion de 0 a 100: ";
     cin >> calificacion;
 
     if (calificacion < 0 || calificacion > 100)
     {
-        cout << "La calificación debe estar entre 0 y 100" << endl;
+        cout << "La calificacion debe estar entre 0 y 100" << endl;
     }
     else if(calificacion >= 95)
     {
-        cout << "Su calificación es: Excelente (E)" << endl;
+        cout << "Su calificacion es: Excelente (E)" << endl;
     }
     else if(calificacion >= 90)
     {
-        cout << "Su calificación es: Muy buena (MB)" << endl;
+        cout << "Su calificacion es: Muy buena (MB)" << endl;
     }
     else if(calificacion >= 80)
     {
-        cout << "Su calificación es: Buena (B)" << endl;
+        cout << "Su calificacion es: Buena (B)" << endl;
     }
     else if(calificacion >= 70)
     {
-        cout << "Su calificación es: Suficiente (S)" << endl;
+        cout << "Su calificacion es: Suficiente (S)" << endl;
     }
     else if(calificacion >= 60)
     {
-        cout << "Su calificación es: Regular (R)" << endl;
+        cout << "Su calificacion es: Regular (R)" << endl;
     }
-    else if(calificacion < 60)
+    else if(calificacion < 60 && calificacion > 0)
     {
-        cout << "Su calificación es: Mala (M)" << endl;
+        cout << "Su calificacion es: Mala (M)" << endl;
     }
-    else
+    else if (calificacion == 0)
     {
         cout << "No entregó o no acreditó (NA)" << endl;
     }
